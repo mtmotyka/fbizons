54cf6cdfc3d888fb42be1ee840f0c54f0114c9cf
Mateusz Motyka <mati@mateuszs-mini.home>
2022-11-29 11:23:14 +0100

    remove unsued import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled, { ThemeProvider } from "styled-components";
 import DetailsBox from "./components/DetailsBox";
 
